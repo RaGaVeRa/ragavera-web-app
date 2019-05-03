373388686fa003a336afa22f069e6edea347f61f
Nikhil DB <nikhildb1997@gmail.com>
2019-05-03 14:24:54 +0530

    Add Angular routes

--- ragavera-web-ui/src/app/app-routing.module.ts
+++ ragavera-web-ui/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TtsComponent } from './tts/tts.component';
+import { OcrComponent } from './ocr/ocr.component';
+
+const routes: Routes = [
+  { path: 'tts', component: TtsComponent },
+  { path: 'ocr', component: OcrComponent },
+  { path: '', redirectTo: '/tts', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
